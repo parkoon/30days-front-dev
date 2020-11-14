4d3e6c085225e5f4a46c68b0f9204aa3d10d9281
parkoon <devparkoon@gmail.com>
2020-11-14 23:15:56 +0900

    chore: add sample components

--- day-01/src/App.tsx
+++ day-01/src/App.tsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Button from './components/Button'
+import TextInput from './components/TextInput'
 
 function App() {
   return (
     <div>
-      <Button>테스트!</Button>
-    </div>
+      <Button>login</Button>
+      <Button outlined>login</Button>
+
+      <TextInput icon="email" type="text" placeholder="Email"/>
+      <TextInput icon="password" type="text" placeholder="Password"/>
+      <TextInput icon="username" type="text" placeholder="Username"/>
+
+
+    </div> 
   )
 }
 
